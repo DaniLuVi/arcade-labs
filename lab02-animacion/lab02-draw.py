e4a300c2213319dc90e195dd643f77f1dc6ca55c
DaniLuVi <dannlucc.07@gmail.com>
2026-02-19 16:10:45 +0100

    coche dibujado

--- lab02-animacion/lab02-draw.py
+++ lab02-animacion/lab02-draw.py
@@ -1,14 +1,42 @@
 import arcade
 
-arcade.open_window(800, 600, "Drawing Example")
+# Defino una clase que dibuje un coche
+class Ventana(arcade.Window):
+    def __init__(self):
+        super().__init__(800, 600, "Animación")
+        arcade.set_background_color((245, 141, 66))
 
-arcade.set_background_color((245, 141, 66))
-arcade.start_render()
+    def extremos_coche(self):
+        arcade.draw_triangle_filled(100, 100, 100, 150, 50, 100, (37, 38, 41))
+        arcade.draw_triangle_filled(200, 100, 200, 150, 250, 100, (37, 38, 41))
+
+    def ruedas_coche(self, num, radio):
+            for i in range(num):
+                arcade.draw_circle_filled(115 + i*80, 100, radio, (37, 38, 41))
+            
+    #def ventanas_coche(self):
+     #   arcade.draw_polygon_filled
+
+    def on_draw(self):
+        self.clear()
+
+        arcade.draw_lbwh_rectangle_filled(100, 100, 100, 50, (37, 38, 41))
+        self.extremos_coche()
+        self.ruedas_coche(2, 20) 
+    
+    
+
+#arcade.open_window(800, 600, "Drawing Example")
+
+#arcade.start_render()
 
 # Dibujo
+
+ventana = Ventana()
+#coche.on_draw()
 
 # --- Finish drawing ---
 
 # Keep the window up until someone closes it.
-arcade.finish_render()
+#arcade.finish_render()
 arcade.run()
