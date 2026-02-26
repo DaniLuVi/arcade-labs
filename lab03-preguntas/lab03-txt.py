5be11c12753b3241802216325e0158d7f1184632
DaniLuVi <dannlucc.07@gmail.com>
2026-02-26 16:07:59 +0100

    realizado un juego de preguntas funcional con txt

--- lab03-preguntas/lab03-txt.py
+++ lab03-preguntas/lab03-txt.py
@@ -11,20 +11,37 @@ def extraer_pregunta(pregunta: str) -> dict:
     return pregunta_dict
 
 def juego_preguntas(preguntas: list):
-    opciones = ["a)", "b)", "c)", "d)"]
+    opciones = ["a", "b", "c", "d"]
     puntos = 0
     for pregunta in preguntas:
         print(pregunta["pregunta"])
         for i in range(len(pregunta["opciones"])):
-            print(f"{opciones[i]} {pregunta["opciones"][i]}")
-            scan = input("Seleccione la respuesta correcta: ")
-            if scan == pregunta["correcta"]:
-                puntos += 5
+            print(f"{opciones[i]}) {pregunta["opciones"][i]}")
 
-with open(path, "r") as file:
-    preguntas = []
-    for line in file:
-        print(line.strip())
-        line = extraer_pregunta(line.strip())
-        print(line)
-        preguntas.append(line)
+        scan = input("Seleccione la respuesta correcta: ")
+        for i in opciones:
+            if scan == i:
+                scan = pregunta["opciones"][opciones.index(i)]
+                break
+        if scan == pregunta["correcta"]:
+            puntos += 5
+            print("¡Respuesta correcta!")
+        else:
+            print("Respuesta incorrecta.")
+
+    if puntos >= 15:
+        print(f"Enhorabuena, has obtenido {puntos} puntos")
+    else:
+        print(f"Vaya, tu puntuación ha sido de {puntos} puntos")
+
+
+if __name__ == "__main__":
+    with open(path, "r") as file:
+        preguntas = []
+        for line in file:
+            print(line.strip())
+            line = extraer_pregunta(line.strip())
+            print(line)
+            preguntas.append(line)
+
+        juego_preguntas(preguntas)
